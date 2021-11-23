0f2d5ac32e9d7408cb4ab957200803aea0fc6806
monica <mnabais@advicefront.com>
2021-11-23 13:11:45 +0000

    Added VideoDetail component, created functionality to embed youtube into the page

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return <VideoItem video={video} onVideoSelect={onVideoSelect} key={video.id.videoId} />;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>
